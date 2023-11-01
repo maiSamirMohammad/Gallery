f1e1a60a116905299544e295266c31b24cb696e9
maiSamirMohammad <88589466+maiSamirMohammad@users.noreply.github.com>
2023-11-02 00:12:16 +0200

    feat: add navigation component

---
+++ app/src/main/java/com/example/retrofit/presentation/MyAlbumDiffCallback.kt
@@ -0,0 +1,21 @@
+package com.example.retrofit.presentation
+
+import androidx.recyclerview.widget.DiffUtil
+
+class MyAlbumDiffCallback: DiffUtil.Callback() {
+    override fun getOldListSize(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun getNewListSize(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+        TODO("Not yet implemented")
+    }
+}
